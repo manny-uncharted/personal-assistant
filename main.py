4c3cbc9f81931afb715752f7c7a1918d914bc1bc
manny-uncharted <omoebun52@gmail.com>
2021-12-28 15:59:04 +0100

    fixed the  bug the program is now working

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import speech_recognition as sr
 from random import choice
 from utils import opening_text
 
+#importing all our functions from the functions folder
+from functions.online_ops import *
+from functions.os_ops import *
+from pprint import pprint
+
 
 USERNAME = config('USER')
 BOTNAME = config('ASSISTANTNAME')
@@ -27,14 +32,19 @@ def greet_user():
     """Sends greeting message to user according to the time"""
 
     hour = datetime.now().hour
+    minute = datetime.now().minute
     if (hour >= 6) and (hour < 12):
-        speak(f"Good Morning {USERNAME}. The time is {hour}")
+        speak(f"Good Morning {USERNAME}. The time is {hour} hours and {minute} minutes ")
     elif (hour >= 12) and (hour <= 16):
-        speak(f"Good afternoon {USERNAME}. The time is {hour}")
+        speak(f"Good afternoon {USERNAME}. The time is {hour} hours and {minute} minutes ")
     elif (hour >=16) and (hour <=19):
-        speak(f"Good evening {USERNAME}. The time is {hour}")
+        speak(f"Good evening {USERNAME}. The time is {hour} hours and {minute} minutes ")
     speak(f"I am {BOTNAME} your personal assistant. How may I assist you?")
 
+# To call the bot to attention
+def call_bot():
+    speak(f" Hello {USERNAME} you called for me! How may I be of help")
+    
 
 # Text to speech conversion
 def speak(text):
@@ -76,3 +86,115 @@ def take_user_input():
         query = 'None'
     return query
     
+"""Here within the main method we initialized the greet function to greet the user
+Then we ran a loop that continously takes input from the user using the take_user_input() function"""
+if __name__ == '__main__':
+    greet_user()
+    while True:
+        query = take_user_input().lower()
+
+        # if f'Hey {BOTNAME}' in query:
+        #     call_bot()
+
+        if 'open notepad ' in query:
+            open_notepad()
+
+        elif 'open calculator' in query:
+            open_calculator()
+        
+        elif 'open command prompt' in query or 'open cmd ' in query:
+            open_cmd()
+        
+        elif 'open camera' in query:
+            open_camera()
+
+        elif 'open discord' in query:
+            open_discord()
+
+        elif 'open excel' in query or 'open microsoft excel' in query:
+            open_excel()
+
+        elif 'open word' in query or 'open microsoft word' in query:
+            open_microsoft_word()
+
+        elif 'open slack' in query:
+            open_slack()
+
+        elif 'ip address' in query:
+            ip_address = find_my_ip()
+            speak(f'Your IP Address is {ip_address}.\n For your convenience, I am printing it on the screen sir.')
+            print(f'Your IP Address is {ip_address}')
+
+        elif 'wikipedia' in query:
+            speak('What do you want to search on Wikipedia, sir?')
+            search_query = take_user_input().lower()
+            results = search_on_wikipedia(search_query)
+            speak(f"According to Wikipedia, {results}")
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(results)
+
+        elif 'youtube' in query:
+            speak('What do you want to play on Youtube, sir?')
+            video = take_user_input().lower()
+            play_on_youtube(video)
+
+        elif 'search on google' in query:
+            speak('What do you want to search on Google, sir?')
+            query = take_user_input().lower()
+            search_on_google(query)
+
+        elif "send whatsapp message" in query:
+            speak('On what number should I send the message sir? Please enter in the console: ')
+            number = input("Enter the number: ")
+            speak("What is the message sir?")
+            message = take_user_input().lower()
+            send_whatsapp_message(number, message)
+            speak("I've sent the message sir.")
+
+        elif "send an email" in query:
+            speak("On what email address do I send sir? Please enter in the console: ")
+            receiver_address = input("Enter email address: ")
+            speak("What should be the subject sir?")
+            subject = take_user_input().capitalize()
+            speak("What is the message sir?")
+            message = take_user_input().capitalize()
+            if send_email(receiver_address, subject, message):
+                speak("I've sent the email sir.")
+            else:
+                speak("Something went wrong while I was sending the mail. Please check the error logs sir.")
+
+        elif 'joke' in query:
+            speak(f"Hope you like this one sir")
+            joke = get_random_joke()
+            speak(joke)
+            speak("For your convenience, I am printing it on the screen sir.")
+            pprint(joke)
+
+        elif "advice" in query:
+            speak(f"Here's an advice for you, sir")
+            advice = get_random_advice()
+            speak(advice)
+            speak("For your convenience, I am printing it on the screen sir.")
+            pprint(advice)
+
+        elif "trending movies" in query:
+            speak(f"Some of the trending movies are: {get_trending_movies()}")
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(*get_trending_movies(), sep='\n')
+
+        elif 'news' in query:
+            speak(f"I'm reading out the latest news headlines, sir")
+            speak(get_latest_news())
+            speak("For your convenience, I am printing it on the screen sir.")
+            print(*get_latest_news(), sep='\n')
+
+        elif 'weather' in query:
+            ip_address = find_my_ip()
+            city = requests.get(f"https://ipapi.co/{ip_address}/city/").text
+            speak(f"Getting weather report for your city {city}")
+            weather, temperature, feels_like = get_weather_report(city)
+            speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
+            speak(f"Also, the weather report talks about {weather}")
+            speak("For your convenience, I am printing it on the screen sir.")
+
+            
